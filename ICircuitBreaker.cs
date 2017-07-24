d95664ca1c54b92b7c24aa0e4fc88cc0b204a064
Leonardo <leonardo@kinetica-solutions.com>
2017-07-24 23:10:49 +0200

    initial commit

---
+++ ICircuitBreaker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCircuitBreaker.Simple
+{
+public interface ICircuitBreaker
+{
+    /// <summary>
+    /// Obtiene la cantidad de fallas aceptadas hasta pasar a estado Open
+    /// </summary>
+    int Threshold { get; }
+
+    /// <summary>
+    /// Obtine el tiempo de espera para pasar a estado Half-Open
+    /// </summary>
+    TimeSpan Timeout { get; }
+    /// <summary>
+    /// Obtiene el estado actual
+    /// </summary>
+    CircuitBreakerState CurrentState { get; }
+    /// <summary>
+    /// Permite invocar una acción utilizado el circuit breaker
+    /// </summary>
+    /// <param name="protectedCode">La acción que será invocada</param>
+    /// <returns>El estado resultante</returns>
+    CircuitBreakerState AttemptCall(Action protectedCode);
+}
+}
---
+++ SimpleCircuitBreaker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetCircuitBreaker.Simple
+{
+public enum CircuitBreakerState
+{
+    Close,
+    Open,
+    HalfOpen
+}
+
+public class SimpleCircuitBreaker : ICircuitBreaker
+{
+    private int threshold;
+    private TimeSpan timeout;
+    private int attemptCounter;
+    private DateTime failureTime;
+    private CircuitBreakerState currentState = CircuitBreakerState.Close;
+
+    public int Threshold => this.threshold;
+    public TimeSpan Timeout => this.timeout;
+    public CircuitBreakerState CurrentState => this.currentState;
+
+    public SimpleCircuitBreaker(int threshold, TimeSpan timeout)
+    {
+        this.threshold = threshold;
+        this.timeout = timeout;
+    }
+
+    public CircuitBreakerState AttemptCall(Action protectedCode)
+    {
+        switch (this.currentState)
+        {
+            case CircuitBreakerState.Close:
+
+                try
+                {
+                    protectedCode();
+                }
+                catch (Exception)
+                {
+                    this.attemptCounter++;
+                    if (this.attemptCounter > this.threshold)
+                    {
+                        this.failureTime = DateTime.Now;
+                        this.attemptCounter = 0;
+                        this.currentState = CircuitBreakerState.Open;
+                    }
+                }
+
+                break;
+            case CircuitBreakerState.Open:
+                if (this.failureTime.Add(this.timeout) > DateTime.Now)
+                {
+                    this.currentState = CircuitBreakerState.HalfOpen;
+                }
+                break;
+            case CircuitBreakerState.HalfOpen:
+                try
+                {
+                    protectedCode();
+                }
+                catch (Exception)
+                {
+                    this.currentState = CircuitBreakerState.Open;
+                }
+                break;
+        }
+
+        return this.currentState;
+    }
+}
+}
